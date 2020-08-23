663aa998821dd0eae2eeb62961fbf55144999975
Your Name <you@example.com>
2020-08-23 10:52:45 -0400

    First commit

--- README.md
+++ README.md
@@ -10,3 +10,23 @@
 
 And there you have it, a start to a discord bot, complete with a command handler
 
+# To use
+
+Put commands in the commands folder, name the what you want the command to be. Start the command folder with the code
+```js
+module.exports.run = (client, message, args) => {
+    //Code Here
+}
+```
+There is already a basic ping command set up, so you can see what I mean
+
+
+And for events, you would name the file the event you want to wait for, and start it with this code
+```js
+module.exports = (client, <Event Args>) {
+
+}
+```
+
+
+I think that is it. If you have any problems, you can bring it up in the issues section
--- events/message.js
+++ events/message.js
@@ -1,12 +1,15 @@
-const  { prefix } = require('../config/config.json')
-
 module.exports = (client, message) => {
-    //Leave's if you are in a DM Or if the bot sends a message or if they have admin only mode set to true
+
+    //Gets the prefix with object destructuring 
+    const  { prefix } = require('../config/config.json')
+
+    //Leave's if you are in a DM or if a bot sends a message
     if (message.guild === null || message.author.bot) return;
     
     //Return's if the message does not start with the set prefix
     if (!message.content.startsWith(prefix)) return;
 
+    //Sets some standard var's
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command);
